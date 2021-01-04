437d79fcada260d78e26bd81fee9e338dec3ab68
wurwal <wurwal@gmail.com>
2021-01-04 17:56:55 +0000

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,13 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+$(document).ready(function() {
+    $('#example').DataTable( {
+        "scrollY": 320,
+        "scrollX": true,
+        "searching": true, 
+        "dom": '<<t><"row table-nav"<"col-md-4"i><"col-md-4"l><"col-md-4"p>>>',
+        "pagingType": "simple"
+    } );
+} );
