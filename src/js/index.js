5c521268bfc836df97eb2e2f73d4232130a9083b
wurwal <wurwal@gmail.com>
2021-01-11 18:19:01 +0000

    state swiper navigation

--- src/js/index.js
+++ src/js/index.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
         "scrollX": true,
         "searching": true,
         "dom": '<<t><"row table-nav"<"col-md-4"i><"col-md-4"l><"col-md-4"p>>>',
-        "pagingType": "simple",
+        "pagingType": "full",
         "infoCallback": function( settings, start, end, max, total, pre ) {
   return "Displaying " + "<b>" + start + "</b>" + " of <b>"+ end + " entries</b>";
 },
@@ -28,6 +28,43 @@ $(function() {
     $( "#sortable-filter-by" ).disableSelection();
 });
 
+//swiper
+
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+
+  // Default parameters
+  slidesPerView: 3,
+  spaceBetween: 10,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 60
+    },
+    // when window width is >= 640px
+    1400: {
+      slidesPerView: 4,
+      spaceBetween: 60
+    }
+  },
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    snapOnRelease: 'true'
+  },
+})
 
 //sidebar
 
@@ -37,6 +74,11 @@ $(function() {
 $( ".close-sidebar" ).click(function() {
 
   $(".col-sidebar").toggleClass( "flex-width-0" )
+
+  $(".open-sidebar-icon").toggleClass( "show" )
+
+  $(".close-sidebar-icon").toggleClass( "show" )
+
 
   $( ".col-main-content" ).toggleClass( "col-md-12" )
 
